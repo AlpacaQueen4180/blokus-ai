cc18118ec1de56a26c5c5c0f94b4f3369dd14c56
AlpacaQueen4180 <30415a@gmail.com>
2022-06-02 00:09:10 +0800

    Add some type hinting, format, .gitignore

--- chess.py
+++ chess.py
@@ -1,14 +1,17 @@
 import math
 
-def rotatex(pt, refpt, deg):
+
+def rotateX(pt, refpt, deg):
     return (refpt[0] + (math.cos(math.radians(deg)) * (pt[0] - refpt[0])) + (math.sin(math.radians(deg)) * (pt[1] - refpt[1])))
 
-def rotatey(pt, refpt, deg):
+
+def rotateY(pt, refpt, deg):
     return (refpt[1] + (-math.sin(math.radians(deg))*(pt[0] - refpt[0])) + (math.cos(math.radians(deg)) * (pt[1] - refpt[1])))
 
 
-def rotatep(pt, refpt, deg):
-    return (int(round(rotatex(pt, refpt, deg))), int(round(rotatey(pt, refpt, deg))))
+def rotateP(pt, refpt, deg):
+    return (int(round(rotateX(pt, refpt, deg))), int(round(rotateY(pt, refpt, deg))))
+
 
 class Shape:
     def __init__(self):
@@ -29,8 +32,8 @@ class Shape:
         self.set_points(x, y)
 
     def rotate(self, deg):
-        self.points = [rotatep(pt, self.refpt, deg) for pt in self.points]
-        self.corners = [rotatep(pt, self.refpt, deg) for pt in self.corners]
+        self.points = [rotateP(pt, self.refpt, deg) for pt in self.points]
+        self.corners = [rotateP(pt, self.refpt, deg) for pt in self.corners]
 
     def flip(self, orientation):
         def flip_h(pt):
@@ -41,150 +44,240 @@ class Shape:
         if orientation == 'h':
             self.points = [flip_h(pt) for pt in self.points]
             self.corners = [flip_h(pt) for pt in self.corners]
+
+
 class I1(Shape):
     def __init__(self):
         self.id = 'I1'
         self.size = 1
+
     def set_points(self, x, y):
         self.points = [(x, y)]
-        self.corners = [(x + 1, y + 1), (x - 1, y - 1), (x + 1, y - 1), (x - 1, y + 1)]
+        self.corners = [(x + 1, y + 1), (x - 1, y - 1),
+                        (x + 1, y - 1), (x - 1, y + 1)]
+
+
 class I2(Shape):
     def __init__(self):
         self.id = 'I2'
         self.size = 2
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1)]
-        self.corners = [(x - 1, y - 1), (x + 1, y - 1), (x + 1, y + 2), (x - 1, y + 2)]
+        self.corners = [(x - 1, y - 1), (x + 1, y - 1),
+                        (x + 1, y + 2), (x - 1, y + 2)]
+
+
 class I3(Shape):
     def __init__(self):
         self.id = 'I3'
         self.size = 3
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x, y + 2)]
-        self.corners = [(x - 1, y - 1), (x + 1, y - 1), (x + 1, y + 3), (x - 1, y + 3)]
+        self.corners = [(x - 1, y - 1), (x + 1, y - 1),
+                        (x + 1, y + 3), (x - 1, y + 3)]
+
+
 class I4(Shape):
     def __init__(self):
         self.id = 'I4'
         self.size = 4
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x, y + 2), (x, y + 3)]
-        self.corners = [(x - 1, y - 1), (x + 1, y - 1), (x + 1, y + 4), (x - 1, y + 4)]
+        self.corners = [(x - 1, y - 1), (x + 1, y - 1),
+                        (x + 1, y + 4), (x - 1, y + 4)]
+
+
 class I5(Shape):
     def __init__(self):
         self.id = 'I5'
         self.size = 5
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x, y + 2), (x, y + 3), (x, y + 4)]
-        self.corners = [(x - 1, y - 1), (x + 1, y - 1), (x + 1, y + 5), (x - 1, y + 5)]
+        self.corners = [(x - 1, y - 1), (x + 1, y - 1),
+                        (x + 1, y + 5), (x - 1, y + 5)]
+
+
 class V3(Shape):
     def __init__(self):
         self.id = 'V3'
         self.size = 3
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x + 1, y)]
-        self.corners = [(x - 1, y - 1), (x + 2, y - 1), (x + 2, y + 1), (x + 1, y + 2), (x - 1, y + 2)]
+        self.corners = [(x - 1, y - 1), (x + 2, y - 1),
+                        (x + 2, y + 1), (x + 1, y + 2), (x - 1, y + 2)]
+
+
 class L4(Shape):
     def __init__(self):
         self.id = 'L4'
         self.size = 4
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x, y + 2), (x + 1, y)]
-        self.corners = [(x - 1, y - 1), (x + 2, y - 1), (x + 2, y + 1), (x + 1, y + 3), (x - 1, y + 3)]
+        self.corners = [(x - 1, y - 1), (x + 2, y - 1),
+                        (x + 2, y + 1), (x + 1, y + 3), (x - 1, y + 3)]
+
+
 class Z4(Shape):
     def __init__(self):
         self.id = 'Z4'
         self.size = 4
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x + 1, y + 1), (x - 1, y)]
-        self.corners = [(x - 2, y - 1), (x + 1, y - 1), (x + 2, y), (x + 2, y + 2), (x - 1, y + 2), (x - 2, y + 1)]
+        self.corners = [(x - 2, y - 1), (x + 1, y - 1), (x + 2, y),
+                        (x + 2, y + 2), (x - 1, y + 2), (x - 2, y + 1)]
+
+
 class O4(Shape):
     def __init__(self):
         self.id = 'O4'
         self.size = 4
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x + 1, y + 1), (x + 1, y)]
-        self.corners = [(x - 1, y - 1), (x + 2, y - 1), (x + 2, y + 2), (x - 1, y + 2)]
+        self.corners = [(x - 1, y - 1), (x + 2, y - 1),
+                        (x + 2, y + 2), (x - 1, y + 2)]
+
+
 class L5(Shape):
     def __init__(self):
         self.id = 'L5'
         self.size = 5
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x + 1, y), (x + 2, y), (x + 3, y)]
-        self.corners = [(x - 1, y - 1), (x + 4, y - 1), (x + 4, y + 1), (x + 1, y + 2), (x - 1, y + 2)]
+        self.corners = [(x - 1, y - 1), (x + 4, y - 1),
+                        (x + 4, y + 1), (x + 1, y + 2), (x - 1, y + 2)]
+
+
 class T5(Shape):
     def __init__(self):
         self.id = 'T5'
         self.size = 5
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x, y + 2), (x - 1, y), (x + 1, y)]
-        self.corners = [(x + 2, y - 1), (x + 2, y + 1), (x + 1, y + 3), (x - 1, y + 3), (x - 2, y + 1), (x - 2, y - 1)]
+        self.corners = [(x + 2, y - 1), (x + 2, y + 1), (x + 1, y + 3),
+                        (x - 1, y + 3), (x - 2, y + 1), (x - 2, y - 1)]
+
+
 class V5(Shape):
     def __init__(self):
         self.id = 'V5'
         self.size = 5
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x, y + 2), (x + 1, y), (x + 2, y)]
-        self.corners = [(x - 1, y - 1), (x + 3, y - 1), (x + 3, y + 1), (x + 1, y + 3), (x - 1, y + 3)]
+        self.corners = [(x - 1, y - 1), (x + 3, y - 1),
+                        (x + 3, y + 1), (x + 1, y + 3), (x - 1, y + 3)]
+
+
 class N(Shape):
     def __init__(self):
         self.id = 'N'
         self.size = 5
+
     def set_points(self, x, y):
-        self.points = [(x, y), (x + 1, y), (x + 2, y), (x, y - 1), (x - 1, y - 1)]
-        self.corners = [(x + 1, y - 2), (x + 3, y - 1), (x + 3, y + 1), (x - 1, y + 1), (x - 2, y), (x - 2, y - 2)]
+        self.points = [(x, y), (x + 1, y), (x + 2, y),
+                       (x, y - 1), (x - 1, y - 1)]
+        self.corners = [(x + 1, y - 2), (x + 3, y - 1), (x + 3,
+                                                         y + 1), (x - 1, y + 1), (x - 2, y), (x - 2, y - 2)]
+
+
 class Z5(Shape):
     def __init__(self):
         self.id = 'Z5'
         self.size = 5
+
     def set_points(self, x, y):
-        self.points = [(x, y), (x + 1, y), (x + 1, y + 1), (x - 1, y), (x - 1, y - 1)]
-        self.corners = [(x + 2, y - 1), (x + 2, y + 2), (x, y + 2), (x - 2, y + 1), (x - 2, y - 2), (x, y - 2)]
+        self.points = [(x, y), (x + 1, y), (x + 1, y + 1),
+                       (x - 1, y), (x - 1, y - 1)]
+        self.corners = [(x + 2, y - 1), (x + 2, y + 2), (x, y + 2),
+                        (x - 2, y + 1), (x - 2, y - 2), (x, y - 2)]
+
+
 class T4(Shape):
     def __init__(self):
         self.id = 'T4'
         self.size = 4
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x + 1, y), (x - 1, y)]
-        self.corners = [(x + 2, y - 1), (x + 2, y + 1), (x + 1, y + 2), (x - 1, y + 2), (x - 2, y + 1), (x - 2, y - 1)]
+        self.corners = [(x + 2, y - 1), (x + 2, y + 1), (x + 1, y + 2),
+                        (x - 1, y + 2), (x - 2, y + 1), (x - 2, y - 1)]
+
+
 class P(Shape):
     def __init__(self):
         self.id = 'P'
         self.size = 5
+
     def set_points(self, x, y):
-        self.points = [(x, y), (x + 1, y), (x + 1, y - 1), (x, y - 1), (x, y - 2)]
-        self.corners = [(x + 1, y - 3), (x + 2, y - 2), (x + 2, y + 1), (x - 1, y + 1), (x - 1, y - 3)]
+        self.points = [(x, y), (x + 1, y), (x + 1, y - 1),
+                       (x, y - 1), (x, y - 2)]
+        self.corners = [(x + 1, y - 3), (x + 2, y - 2),
+                        (x + 2, y + 1), (x - 1, y + 1), (x - 1, y - 3)]
+
+
 class W(Shape):
     def __init__(self):
         self.id = 'W'
         self.size = 5
+
     def set_points(self, x, y):
-        self.points = [(x, y), (x, y + 1), (x + 1, y + 1), (x - 1, y), (x - 1, y - 1)]
-        self.corners = [(x + 1, y - 1), (x + 2, y), (x + 2, y + 2), (x - 1, y + 2), (x - 2, y + 1), (x - 2, y - 2), (x, y - 2)]
+        self.points = [(x, y), (x, y + 1), (x + 1, y + 1),
+                       (x - 1, y), (x - 1, y - 1)]
+        self.corners = [(x + 1, y - 1), (x + 2, y), (x + 2, y + 2),
+                        (x - 1, y + 2), (x - 2, y + 1), (x - 2, y - 2), (x, y - 2)]
+
+
 class U(Shape):
     def __init__(self):
         self.id = 'U'
         self.size = 5
+
     def set_points(self, x, y):
-        self.points = [(x, y), (x, y + 1), (x + 1, y + 1), (x, y - 1), (x + 1, y - 1)]
-        self.corners = [(x + 2, y - 2), (x + 2, y), (x + 2, y + 2), (x - 1, y + 2), (x - 1, y - 2)]
+        self.points = [(x, y), (x, y + 1), (x + 1, y + 1),
+                       (x, y - 1), (x + 1, y - 1)]
+        self.corners = [(x + 2, y - 2), (x + 2, y),
+                        (x + 2, y + 2), (x - 1, y + 2), (x - 1, y - 2)]
+
+
 class F(Shape):
     def __init__(self):
         self.id = 'F'
         self.size = 5
+
     def set_points(self, x, y):
-        self.points = [(x, y), (x, y + 1), (x + 1, y + 1), (x, y - 1), (x - 1, y)]
-        self.corners = [(x + 1, y - 2), (x + 2, y), (x + 2, y + 2), (x - 1, y + 2), (x - 2, y + 1), (x - 2, y - 1), (x - 1, y - 2)]
+        self.points = [(x, y), (x, y + 1), (x + 1, y + 1),
+                       (x, y - 1), (x - 1, y)]
+        self.corners = [(x + 1, y - 2), (x + 2, y), (x + 2, y + 2),
+                        (x - 1, y + 2), (x - 2, y + 1), (x - 2, y - 1), (x - 1, y - 2)]
+
+
 class X(Shape):
     def __init__(self):
         self.id = 'X'
         self.size = 5
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y)]
-        self.corners = [(x + 1, y - 2), (x + 2, y - 1), (x + 2, y + 1), (x + 1, y + 2), (x - 1, y + 2), (x - 2, y + 1), (x - 2, y - 1), (x - 1, y - 2)]
+        self.corners = [(x + 1, y - 2), (x + 2, y - 1), (x + 2, y + 1), (x + 1, y + 2),
+                        (x - 1, y + 2), (x - 2, y + 1), (x - 2, y - 1), (x - 1, y - 2)]
+
+
 class Y(Shape):
     def __init__(self):
         self.id = 'Y'
         self.size = 5
+
     def set_points(self, x, y):
         self.points = [(x, y), (x, y + 1), (x + 1, y), (x + 2, y), (x - 1, y)]
-        self.corners = [(x + 3, y - 1), (x + 3, y + 1), (x + 1, y + 2), (x - 1, y + 2), (x - 2, y + 1), (x - 2, y - 1)]
+        self.corners = [(x + 3, y - 1), (x + 3, y + 1), (x + 1, y + 2),
+                        (x - 1, y + 2), (x - 2, y + 1), (x - 2, y - 1)]
--- gamestate.py
+++ gamestate.py
@@ -3,87 +3,113 @@ import math
 import random
 import copy
 import numpy as np
-import chess
+from chess import *
+
+
 class Board:
     def __init__(self, nrow, ncol):
         self.nrow = nrow
         self.ncol = ncol
         self.state = [['_'] * ncol for _ in range(nrow)]
+
     def update(self, player_id, placement):
         for row in range(self.nrow):
             for col in range(self.ncol):
                 if(col, row) in placement:
                     self.state[row][col] = player_id
+
     def in_bounds(self, point):
         return 0 <= point[0] < self.ncol and 0 <= point[1] < self.nrow
+
     def overlap(self, placement):
-        return False in[(self.state[y][x] == '_') for x, y in placement]
+        return False in [(self.state[y][x] == '_') for x, y in placement]
+
     def adj(self, player_id, placement):
         adjacents = []
         for x, y in placement:
-            if self.in_bounds((x + 1, y)) : adjacents += [self.state[y][x + 1] == player_id]
-            if self.in_bounds((x - 1, y)) : adjacents += [self.state[y][x - 1] == player_id]
-            if self.in_bounds((x, y - 1)) : adjacents += [self.state[y - 1][x] == player_id]
-            if self.in_bounds((x, y + 1)) : adjacents += [self.state[y + 1][x] == player_id]
+            if self.in_bounds((x + 1, y)):
+                adjacents += [self.state[y][x + 1] == player_id]
+            if self.in_bounds((x - 1, y)):
+                adjacents += [self.state[y][x - 1] == player_id]
+            if self.in_bounds((x, y - 1)):
+                adjacents += [self.state[y - 1][x] == player_id]
+            if self.in_bounds((x, y + 1)):
+                adjacents += [self.state[y + 1][x] == player_id]
         return True in adjacents
+
     def corner(self, player_id, placement):
         corners = []
         for x, y in placement:
-            if self.in_bounds((x + 1, y + 1)) : corners += [self.state[y + 1][x + 1] == player_id]
-            if self.in_bounds((x - 1, y - 1)) : corners += [self.state[y - 1][x - 1] == player_id]
-            if self.in_bounds((x + 1, y - 1)) : corners += [self.state[y - 1][x + 1] == player_id]
-            if self.in_bounds((x - 1, y + 1)) : corners += [self.state[y + 1][x - 1] == player_id]
+            if self.in_bounds((x + 1, y + 1)):
+                corners += [self.state[y + 1][x + 1] == player_id]
+            if self.in_bounds((x - 1, y - 1)):
+                corners += [self.state[y - 1][x - 1] == player_id]
+            if self.in_bounds((x + 1, y - 1)):
+                corners += [self.state[y - 1][x + 1] == player_id]
+            if self.in_bounds((x - 1, y + 1)):
+                corners += [self.state[y + 1][x - 1] == player_id]
         return True in corners
+
     def print_board(self):
         print("Current Board Layout:")
         for row in range(len(self.state)):
             for col in range(len(self.state[0])):
-                print(" " + str(self.state[row][col]), end = '')
+                print(" " + str(self.state[row][col]), end='')
             print()
 
-class GameState :
-    def __init__(self, playerid, strategy, board) :
+
+class GameState:
+    def __init__(self, playerid, strategy, board):
         self.playerid = playerid                    # player's id
-        self.pieces = []                # player's unused game piece, list of Pieces
+        self.pieces: list[Shape] = []                # player's unused game piece, list of Pieces
         self.corners = set()            # current valid corners on board
         self.strategy = strategy        # player's strategy
         self.score = 0                  # player's current score
         self.is_blocked = False
         self.board = board
-    def getscore(self) :
+
+    def getscore(self):
         return self.score
-    def getunusedpiece(self) :
+
+    def getunusedpiece(self):
         return self.pieces
-    def add_pieces(self, pieces) :
+
+    def add_pieces(self, pieces):
         self.pieces = pieces
-    def remove_piece(self, piece) :
+
+    def remove_piece(self, piece):
         self.pieces = [p for p in self.pieces if p.id != piece.id]
-    def update_player(self, piece, board) :
-        self.score += piece.size # If you're placing your last piece, you get +15 bonus
+
+    def update_player(self, piece: Shape, board: Board):
+        self.score += piece.size  # If you're placing your last piece, you get +15 bonus
         if len(self.pieces) == 1:
             self.score += 15
-            if piece.id == 'Pole': # +5 extra if its the [] piece
+            if piece.id == 'Pole':  # +5 extra if its the [] piece
                 self.score += 5
-        for c in piece.corners: # Add the player's available corners
+        for c in piece.corners:  # Add the player's available corners
             if board.in_bounds(c) and not board.overlap([c]):
                 self.corners.add(c)
-    def getpossiblemoves(self, pieces, game): # Updates the corners of the player, in case the corners have been covered by another player's pieces.
-        self.corners = set([(x, y) for (x, y) in self.corners if game.board.state[y][x] == '_'])
-        placements = [] # a list of possible placements
-        visited = [] # a list placements (a set of points on board)
+
+    # Updates the corners of the player, in case the corners have been covered by another player's pieces.
+    def getpossiblemoves(self, pieces: list[Shape], game):
+        self.corners = set(
+            [(x, y) for (x, y) in self.corners if game.board.state[y][x] == '_'])
+        placements = []  # a list of possible placements
+        visited = []  # a list placements (a set of points on board)
         for cr in self.corners:          # Check every available corner
             for sh in pieces:            # Check every available piece
-                for num in range(sh.size):                          # Check every reference point the piece could have
+                # Check every reference point the piece could have
+                for num in range(sh.size):
                     for flip in ["h", "v"]:                         # Check every flip
                         for rot in [0, 90, 180, 270]:               # Check every rotation
-                            candidate = copy.deepcopy(sh)           # Create a copy to prevent an overwrite on the original
+                            # Create a copy to prevent an overwrite on the original
+                            candidate = copy.deepcopy(sh)
                             candidate.create(num, cr)
                             candidate.flip(flip)
                             candidate.rotate(rot)
-                            if game.valid_move(self, candidate.points): # If the placement is valid and new
+                            # If the placement is valid and new
+                            if game.valid_move(self, candidate.points):
                                 if not set(candidate.points) in visited:
                                     placements.append(candidate)
                                     visited.append(set(candidate.points))
         return placements
-    
-
